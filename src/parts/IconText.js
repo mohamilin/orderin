03db2ec3231c30f01bc49070fc8ba15d05628de6
Moh Amilin <amilinmoh99@gmail.com>
2021-06-30 05:11:43 +0700

    #### Membuat logo orderine

---
+++ src/parts/IconText.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "components/button";
+export default function IconText() {
+  return (
+    <div>
+      <Button className="brand-text-logo" href="" type="link">
+        Order<span className="text-color-orange">ine</span>
+      </Button>
+    </div>
+  );
+}
