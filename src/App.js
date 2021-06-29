340b4594317f0575961a5b45909ed1c52c816991
Moh Amilin <amilinmoh99@gmail.com>
2021-06-30 05:11:13 +0700

    test : import icon text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import LandingPage from "pages/LandingPage";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+
 import "./App.css";
 import "./assets/scss/style.scss";
 import Button from "./components/button";
+import IconText from "./parts/IconText";
 function App() {
   return (
     <div className="App">
       <Button isPrimary> hai </Button>
+      <Router>
+        <LandingPage />
+      </Router>
     </div>
   );
 }
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import IconText from "parts/IconText";
+import React from "react";
+
+export default function LandingPage() {
+  return (
+    <>
+      <IconText />
+    </>
+  );
+}
